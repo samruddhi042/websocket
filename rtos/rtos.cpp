cadba520934ae6a3978b74543f4d0cf153731470
Samruddhi Chavan <samruddhic006@gmail.com>
2026-05-05 15:33:47 +0530

    Fixed rtos.cpp

--- rtos/rtos.cpp
+++ rtos/rtos.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include "config.h"
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
 	std::cout <<"Hello World" << std::endl;
-	std::cout <<"Version " << rtos_VERSION_MAJOR << "." << rtos_VERSION_MINOR <<std::endl;
+	std::cout <<"Version " << rtos_VERSION_MAJOR <<"."<< rtos_VERSION_MINOR <<std::endl;
 	return 0;
 	
 }
